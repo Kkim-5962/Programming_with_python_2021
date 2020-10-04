ba90aecddb58fa0c99db570a6b0b3eccbc6283c9
MOONHAB <43397401+MOONHAB@users.noreply.github.com>
2020-10-04 23:04:27 +0200

    update
    Update

---
+++ Kim KH/my_city.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+##################################################
+# This script shows an example of a header section. This sections is a group of commented lines (lines starting with
+# the "#" character) that describes general features of the script such as the type of licence (defined by the
+# developer), authors, credits, versions among others
+##################################################
+#
+##################################################
+# Author: Diego Pajarito
+# Copyright: Copyright 2020, IAAC
+# Credits: [Institute for Advanced Architecture of Catalonia - IAAC, Advanced Architecture group]
+# Version: 1.0.0
+# Maintainer: Kyunghwan KIM
+# Email: kyunghwan.kim@students.iaac.net
+# Status: development
+##################################################
+
+# End of header section
+
+city_name = 'Seoul'
+city_area = 200000
+city_population = 3000
+city_density = city_population / city_area
+
+print('population Density')
+print(city_density)
+
+x = 1 * 2 + 6 / 3
+print(x)
+
+x 1!= 2
